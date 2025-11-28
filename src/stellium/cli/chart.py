40c84389a591a53ac20b07c2e798231aca35054c
Kate Louie <katehlouie@gmail.com>
2025-11-27 22:52:28 -0800

    Add CLI tests

--- src/stellium/cli/chart.py
+++ src/stellium/cli/chart.py
@@ -35,16 +35,31 @@ def chart_from_registry_cmd(name, output, house_system, output_format):
     """
     from stellium.core.builder import ChartBuilder
     from stellium.data.registry import get_notable_registry
+    from stellium.engines.houses import (
+        EqualHouses,
+        KochHouses,
+        PlacidusHouses,
+        WholeSignHouses,
+    )
     from stellium.presentation.builder import ReportBuilder
+
+    # Map house system name to engine instance
+    house_engines = {
+        "Placidus": PlacidusHouses,
+        "Whole Sign": WholeSignHouses,
+        "Koch": KochHouses,
+        "Equal": EqualHouses,
+    }
 
     try:
         registry = get_notable_registry()
         notable = registry.get_by_name(name)
         # Build chart
         if notable:
+            house_engine = house_engines[house_system]()
             chart = (
                 ChartBuilder.from_native(notable)
-                .with_house_systems([house_system])
+                .with_house_systems([house_engine])
                 .calculate()
             )
         else:
